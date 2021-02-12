4ef96abcd50905a5cb8a54559ad957d96e936427
Zhafranth <zhafranth@gmail.com>
2021-02-12 14:48:20 +0700

    frontend build

---
+++ precache-manifest.6b2de04f7aca4cb5eb3545ab8096f3e0.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00837dbfa59745c60f361f919b61094a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab09fd05d8e5c0196a36",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "97107073bfaab56d57ee",
+    "url": "/static/css/main.fb2ef378.chunk.css"
+  },
+  {
+    "revision": "ab09fd05d8e5c0196a36",
+    "url": "/static/js/2.80b23284.chunk.js"
+  },
+  {
+    "revision": "cb6a4b5dabe3dddfa89f8414b4b6d1f4",
+    "url": "/static/js/2.80b23284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97107073bfaab56d57ee",
+    "url": "/static/js/main.c62336a9.chunk.js"
+  },
+  {
+    "revision": "ed97526229765ec5e48b",
+    "url": "/static/js/runtime-main.b5c22d83.js"
+  },
+  {
+    "revision": "6186e9ab30df4137f08e86706b2ed45c",
+    "url": "/static/media/5.6186e9ab.mp4"
+  },
+  {
+    "revision": "fcebd9fe5e7f8d8713ffd46a62e7a149",
+    "url": "/static/media/Jumbotron.fcebd9fe.png"
+  },
+  {
+    "revision": "a67bfa6e6e4ca5166259dc5a638cdad8",
+    "url": "/static/media/about1.a67bfa6e.png"
+  },
+  {
+    "revision": "a9ef94f5b6cb2d759a0dbae574c55885",
+    "url": "/static/media/about2.a9ef94f5.png"
+  },
+  {
+    "revision": "b847bee4d23fa7035c6df1ee6859fd12",
+    "url": "/static/media/bakdrop-login.b847bee4.png"
+  },
+  {
+    "revision": "6768dca07b86295b1ca16d38cf3ee399",
+    "url": "/static/media/benefit1.6768dca0.png"
+  },
+  {
+    "revision": "8c972abd49e24e8547f3ebe400acebe8",
+    "url": "/static/media/benefit2.8c972abd.png"
+  },
+  {
+    "revision": "76a312a9008026efcbb2f90c5d870f5c",
+    "url": "/static/media/benefit3.76a312a9.png"
+  },
+  {
+    "revision": "c4d28e7dfc92311ddd7e45491c6a835f",
+    "url": "/static/media/contact.c4d28e7d.png"
+  },
+  {
+    "revision": "230e4d21b02ac85f9afd875714f42e6d",
+    "url": "/static/media/empty.230e4d21.png"
+  },
+  {
+    "revision": "6bd72e94461ba8199025139c906ff9df",
+    "url": "/static/media/facebook.6bd72e94.svg"
+  },
+  {
+    "revision": "800a5cdc9ff25aafe32ccf759203eb53",
+    "url": "/static/media/hero-kategori.800a5cdc.png"
+  },
+  {
+    "revision": "e2752b36b4fdb104566070085c02c281",
+    "url": "/static/media/icon-social1.e2752b36.svg"
+  },
+  {
+    "revision": "358f0b84a3dd7338ae8e31a5104ceeb5",
+    "url": "/static/media/icon-social2.358f0b84.svg"
+  },
+  {
+    "revision": "6ff601328e599c2c476536c5cf7aac49",
+    "url": "/static/media/illustration.6ff60132.png"
+  },
+  {
+    "revision": "637fa159ec1371ce39dd4855bea2e087",
+    "url": "/static/media/img-card-progress.637fa159.png"
+  },
+  {
+    "revision": "48fef59b9ed24ec41545f6b076d15675",
+    "url": "/static/media/instagram.48fef59b.svg"
+  },
+  {
+    "revision": "3da08d165901c9d02dd4c9a6cbc4f792",
+    "url": "/static/media/kategori-icon1.3da08d16.svg"
+  },
+  {
+    "revision": "209ec9cac6219ad2e8dacc4582f3f4f6",
+    "url": "/static/media/list-materi-icon.209ec9ca.svg"
+  },
+  {
+    "revision": "fa7a3966e9cfdca560ee17b1d7851ba3",
+    "url": "/static/media/maintenance-page.fa7a3966.png"
+  },
+  {
+    "revision": "492506e7aa80380bdb1245f9792ecb02",
+    "url": "/static/media/orderclass.492506e7.png"
+  },
+  {
+    "revision": "c3bc5852453c64e4a8c69a532bec934e",
+    "url": "/static/media/profile-page-image.c3bc5852.png"
+  },
+  {
+    "revision": "96c6aa5e2175aa803494d1b1b2cbf875",
+    "url": "/static/media/profile-pic.96c6aa5e.png"
+  },
+  {
+    "revision": "e74e0ee2d21979decac4e5ac8f8f1165",
+    "url": "/static/media/promosi.e74e0ee2.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d47c25d413ee583ea65e154b109d84f2",
+    "url": "/static/media/user-testimoni.d47c25d4.png"
+  },
+  {
+    "revision": "6b15e8a275ea446b74a7eb9389d4cf78",
+    "url": "/static/media/video-icon.6b15e8a2.svg"
+  },
+  {
+    "revision": "b77da672fb3d2674952ffe8c9f4794a1",
+    "url": "/static/media/video-overview.b77da672.png"
+  }
+]);
